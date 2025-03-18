58c96d71cc6354f86401401a71525edca1e763bf
Shanmathi <shanmathiganeshbabu@gmail.com>
2025-03-18 10:15:03 +0530

    Update HomeRunner.java

--- src/test/java/runners/HomeRunner.java
+++ src/test/java/runners/HomeRunner.java
@@ -7,16 +7,8 @@ import io.cucumber.testng.CucumberOptions;
         features = "src/test/resources/feature/Home.feature",
         glue = "stepDefinition",
         tags="@home",
-<<<<<<< HEAD
-	plugin = {"pretty", "json:target/cucumber.json", "html:target/cucumber-reports.html"}
-)
-public class HomeRunner extends AbstractTestNGCucumberTests{
-	
-}
-=======
         plugin = {"pretty", "json:target/cucumber.json", "html:target/cucumber-reports.html"}
 )
 public class HomeRunner extends AbstractTestNGCucumberTests{
 	
 }
->>>>>>> ef3c05e (Initial commit - Uploading Bookswagon Automation project)
