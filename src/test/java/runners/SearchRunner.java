a50c4c69da619d8648f31e437b8e9e5c7a96e631
Shanmathi <shanmathiganeshbabu@gmail.com>
2025-03-18 11:24:51 +0530

    Update SearchRunner.java

--- src/test/java/runners/SearchRunner.java
+++ src/test/java/runners/SearchRunner.java
@@ -6,7 +6,7 @@ import io.cucumber.testng.CucumberOptions;
 @CucumberOptions(
         features = "src/test/resources/feature/Search.feature",
         glue = "stepDefinition",
-        plugin = {"pretty", "json:target/cucumber.json", "html:target/cucumber-reports.html"}
+        plugin = {"pretty", "json:target/cucumber-reports/CucumberTestReport.json", "html:target/cucumber-reports.html"}
 )
 public class SearchRunner extends AbstractTestNGCucumberTests{
 	
